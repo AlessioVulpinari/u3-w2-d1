ae5fd8d56f13d79f30510ebcc48ec254b8c942ed
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-04-29 14:33:08 +0200

    initial commit

---
+++ src/components/SingleBooks.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react"
+import { Badge, Card, Col } from "react-bootstrap"
+
+class SingleBooks extends Component {
+  state = { selected: false }
+
+  render() {
+    return (
+      <Col key={this.props.bookIns.asin}>
+        <Card className={this.state.selected && "border border-danger"}>
+          <Card.Img
+            variant='top'
+            src={this.props.bookIns.img}
+            onClick={() => {
+              this.setState({ selected: !this.state.selected })
+            }}
+          />
+          <Card.Body className='d-flex flex-column justify-content-between bg-secondary'>
+            <Card.Title>{this.props.bookIns.title}</Card.Title>
+            <Card.Text className='d-flex justify-content-between align-items-center'>
+              <span>{this.props.bookIns.category.charAt(0).toUpperCase() + this.props.bookIns.category.slice(1)}</span>
+              <Badge bg='success'>{this.props.bookIns.price}</Badge>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  }
+}
+
+export default SingleBooks
